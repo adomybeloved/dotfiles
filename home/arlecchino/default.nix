20380238b0073207d7bd388ab0e5904cfd410c9f
Aleksandra Mironova <51828284+adomybeloved@users.noreply.github.com>
2026-08-22 00:39:14 +0300

    fix(gtk): relocate gtk2 config to .config/gtk-2.0/gtkrc to permanently resolve clobber conflict

--- home/arlecchino/default.nix
+++ home/arlecchino/default.nix
@@ -23,12 +23,8 @@
 
   gtk = {
     enable = true;
+    gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
   };
-
-  home.file.".gtkrc-2.0".force = true;
-  xdg.configFile."gtk-3.0/settings.ini".force = true;
-  xdg.configFile."gtk-4.0/settings.ini".force = true;
-  xdg.configFile."mimeapps.list".force = true;
 
   home.packages = with pkgs; [
     antigravity-ide
