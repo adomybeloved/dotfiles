74b675714d274681d81ec5bcc96e519cbb2aaf0b
Aleksandra Mironova <51828284+adomybeloved@users.noreply.github.com>
2026-08-22 01:37:23 +0300

    fix(qylock): wrap qylock-lock with QT_PLUGIN_PATH and GST_PLUGIN_SYSTEM_PATH_1_0 for video playback in lockscreen

--- modules/desktop/qylock.nix
+++ modules/desktop/qylock.nix
@@ -1,5 +1,44 @@
 { config, pkgs, ... }:
 
+let
+  gstPlugins = with pkgs.gst_all_1; [
+    gstreamer
+    gst-plugins-base
+    gst-plugins-good
+    gst-plugins-bad
+    gst-plugins-ugly
+    gst-libav
+  ];
+
+  gstPluginPath = pkgs.lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" gstPlugins;
+
+  qtPluginPath = pkgs.lib.makeSearchPathOutput "lib" "lib/qt-6/plugins" [
+    pkgs.qt6.qtbase
+    pkgs.qt6.qtdeclarative
+    pkgs.qt6.qtmultimedia
+    pkgs.qt6.qtwayland
+    pkgs.qt6.qtsvg
+  ];
+
+  qmlImportPath = pkgs.lib.makeSearchPathOutput "lib" "lib/qt-6/qml" [
+    pkgs.qt6.qtdeclarative
+    pkgs.qt6.qt5compat
+    pkgs.qt6.qtmultimedia
+    pkgs.qt6.qtsvg
+    pkgs.quickshell
+  ];
+
+  qylockLockWrapped = pkgs.writeShellScriptBin "qylock-lock" ''
+    export GST_PLUGIN_SYSTEM_PATH_1_0="${gstPluginPath}"
+    export QT_PLUGIN_PATH="${qtPluginPath}''${QT_PLUGIN_PATH:+:$QT_PLUGIN_PATH}"
+    export QML_IMPORT_PATH="${qmlImportPath}''${QML_IMPORT_PATH:+:$QML_IMPORT_PATH}"
+    export QML2_IMPORT_PATH="${qmlImportPath}''${QML2_IMPORT_PATH:+:$QML2_IMPORT_PATH}"
+    export XDG_SESSION_TYPE="wayland"
+    export QT_QPA_PLATFORM="wayland"
+    export QS_THEME="''${1:-R1999_2}"
+    exec /run/current-system/sw/bin/.qylock-lock-wrapped "$@" 2>/dev/null || exec quickshell -p /run/current-system/sw/share/qylock/lock_shell.qml
+  '';
+in
 {
   programs.qylock = {
     enable = true;
@@ -25,16 +64,15 @@
   };
 
   environment.sessionVariables = {
-    GST_PLUGIN_SYSTEM_PATH_1_0 = "/run/current-system/sw/lib/gstreamer-1.0";
+    GST_PLUGIN_SYSTEM_PATH_1_0 = "${gstPluginPath}";
+    QT_PLUGIN_PATH = "${qtPluginPath}";
+    QML2_IMPORT_PATH = "${qmlImportPath}";
+    QML_IMPORT_PATH = "${qmlImportPath}";
   };
 
-  environment.systemPackages = with pkgs; [
-    gst_all_1.gstreamer
-    gst_all_1.gst-plugins-base
-    gst_all_1.gst-plugins-good
-    gst_all_1.gst-plugins-bad
-    gst_all_1.gst-plugins-ugly
-    gst_all_1.gst-libav
-    ffmpeg
-  ];
+  environment.systemPackages = [
+    qylockLockWrapped
+    pkgs.quickshell
+    pkgs.ffmpeg
+  ] ++ gstPlugins;
 }
