7120c61aa453c2fc9bd8723b9324e444e468db5f
Aleksandra Mironova <51828284+adomybeloved@users.noreply.github.com>
2026-08-22 00:16:41 +0300

    feat(desktop): add catppuccin-kde, catppuccin-gtk, cursors, and kvantum packages

--- modules/desktop/plasma.nix
+++ modules/desktop/plasma.nix
@@ -12,6 +12,28 @@
     options = "grp:alt_shift_toggle";
   };
 
+  environment.systemPackages = with pkgs; [
+    (catppuccin-kde.override {
+      flavour = [ "mocha" ];
+      accents = [ "mauve" ];
+      winDecStyles = [ "modern" ];
+    })
+    (catppuccin-papirus-folders.override {
+      flavor = "mocha";
+      accent = "mauve";
+    })
+    (catppuccin-gtk.override {
+      accents = [ "mauve" ];
+      size = "standard";
+      variant = "mocha";
+    })
+    (catppuccin-kvantum.override {
+      accent = "mauve";
+    })
+    catppuccin-cursors.mochaMauve
+    catppuccin-cursors.mochaDark
+  ];
+
   catppuccin = {
     enable = true;
     autoEnable = true;
