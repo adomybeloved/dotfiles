4537d40f394109e1ad6bff95ae52dc585db81506
Aleksandra Mironova <51828284+adomybeloved@users.noreply.github.com>
2026-08-22 01:40:04 +0300

    fix(qylock): properly build and wrap qylock-quickshell with Qt6Multimedia and GStreamer plugin paths

--- modules/desktop/qylock.nix
+++ modules/desktop/qylock.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 let
   gstPlugins = with pkgs.gst_all_1; [
@@ -12,39 +12,68 @@ let
 
   gstPluginPath = pkgs.lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" gstPlugins;
 
-  qtPluginPath = pkgs.lib.makeSearchPathOutput "lib" "lib/qt-6/plugins" [
-    pkgs.qt6.qtbase
-    pkgs.qt6.qtdeclarative
-    pkgs.qt6.qtmultimedia
-    pkgs.qt6.qtwayland
-    pkgs.qt6.qtsvg
-  ];
+  qmlPath = "${pkgs.qt6.qt5compat}/lib/qt-6/qml:${pkgs.qt6.qtdeclarative}/lib/qt-6/qml:${pkgs.qt6.qtmultimedia}/lib/qt-6/qml:${pkgs.qt6.qtsvg}/lib/qt-6/qml:${pkgs.quickshell}/lib/qt-6/qml";
 
-  qmlImportPath = pkgs.lib.makeSearchPathOutput "lib" "lib/qt-6/qml" [
-    pkgs.qt6.qtdeclarative
-    pkgs.qt6.qt5compat
-    pkgs.qt6.qtmultimedia
-    pkgs.qt6.qtsvg
-    pkgs.quickshell
-  ];
+  qtPluginPath = "${pkgs.qt6.qtmultimedia}/lib/qt-6/plugins:${pkgs.qt6.qtbase}/lib/qt-6/plugins:${pkgs.qt6.qtwayland}/lib/qt-6/plugins";
 
-  qylockLockWrapped = pkgs.writeShellScriptBin "qylock-lock" ''
-    export GST_PLUGIN_SYSTEM_PATH_1_0="${gstPluginPath}"
-    export QT_PLUGIN_PATH="${qtPluginPath}''${QT_PLUGIN_PATH:+:$QT_PLUGIN_PATH}"
-    export QML_IMPORT_PATH="${qmlImportPath}''${QML_IMPORT_PATH:+:$QML_IMPORT_PATH}"
-    export QML2_IMPORT_PATH="${qmlImportPath}''${QML2_IMPORT_PATH:+:$QML2_IMPORT_PATH}"
-    export XDG_SESSION_TYPE="wayland"
-    export QT_QPA_PLATFORM="wayland"
-    export QS_THEME="''${1:-R1999_2}"
-    exec /run/current-system/sw/bin/.qylock-lock-wrapped "$@" 2>/dev/null || exec quickshell -p /run/current-system/sw/share/qylock/lock_shell.qml
-  '';
+  qylockPkg = pkgs.stdenvNoCC.mkDerivation {
+    pname = "qylock-quickshell";
+    version = "unstable";
+    src = inputs.qylock;
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    dontBuild = true;
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out/share/qylock
+      cp -r quickshell-lockscreen/. $out/share/qylock/
+      cp -r themes $out/share/qylock/themes
+
+      mkdir -p $out/bin
+      makeWrapper $out/share/qylock/lock.sh $out/bin/qylock-lock \
+        --set-default QS_THEME "R1999_2" \
+        --set QYLOCK_THEMES_ROOT "$out/share/qylock/themes" \
+        --set XDG_SESSION_TYPE "wayland" \
+        --set QT_QPA_PLATFORM "wayland" \
+        --suffix QML2_IMPORT_PATH : "${qmlPath}" \
+        --suffix QML_IMPORT_PATH : "${qmlPath}" \
+        --suffix QT_PLUGIN_PATH : "${qtPluginPath}" \
+        --suffix GST_PLUGIN_SYSTEM_PATH_1_0 : "${gstPluginPath}" \
+        --prefix PATH : ${pkgs.lib.makeBinPath [
+          pkgs.quickshell
+          pkgs.psmisc
+          pkgs.systemd
+          pkgs.coreutils
+        ]}
+
+      substituteInPlace $out/share/qylock/lock.sh \
+        --replace-fail \
+          'CONFIG_FILE="$HOME/.config/qylock/theme"
+if [ -n "$1" ]; then
+    export QS_THEME="$1"
+elif [ -f "$CONFIG_FILE" ]; then
+    export QS_THEME=$(cat "$CONFIG_FILE")
+else
+    export QS_THEME="nier-automata"
+fi' \
+          'if [ -n "$1" ]; then export QS_THEME="$1"; fi' \
+        --replace-fail \
+          'if [ -d "$DIR/../themes" ] && [ ! -d "$DIR/themes_link" ]; then
+    export QS_THEME_PATH="$DIR/../themes/$QS_THEME"
+else
+    export QS_THEME_PATH="$DIR/themes_link/$QS_THEME"
+fi' \
+          'export QS_THEME_PATH="$QYLOCK_THEMES_ROOT/$QS_THEME"'
+
+      runHook postInstall
+    '';
+  };
 in
 {
   programs.qylock = {
     enable = true;
     theme = "R1999_2";
     sddm.enable = true;
-    quickshell.enable = true;
+    quickshell.enable = false;
   };
 
   services.displayManager.sddm = {
@@ -66,13 +95,10 @@ in
   environment.sessionVariables = {
     GST_PLUGIN_SYSTEM_PATH_1_0 = "${gstPluginPath}";
     QT_PLUGIN_PATH = "${qtPluginPath}";
-    QML2_IMPORT_PATH = "${qmlImportPath}";
-    QML_IMPORT_PATH = "${qmlImportPath}";
   };
 
   environment.systemPackages = [
-    qylockLockWrapped
-    pkgs.quickshell
+    qylockPkg
     pkgs.ffmpeg
   ] ++ gstPlugins;
 }
